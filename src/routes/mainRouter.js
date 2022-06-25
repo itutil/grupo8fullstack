957ee790cea629d237707ce88dda53244df05211
Ramon F. Lopez-Imizcoz <ramon@nubimetrics.com>
2022-06-25 20:01:31 -0300

    Minor corrections

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,10 +1,13 @@
 const express = require ('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
+const productController = require('../controllers/productController');
 
 router.get('/', mainController.index);
 router.get('/menu-detail', mainController.mainDetail);
 router.get('/login', mainController.login);
 router.get('/register', mainController.login);
+router.get('/productos', productController.index);
+router.get('/products', productController.index);
 
 module.exports = router;

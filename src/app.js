e9e14a39f5dcf4724e78d1dd2572b972430e505a
GuillermoFontana <guilleefontana@gmail.com>
2022-06-24 21:30:50 -0300

    methodOverride

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const app = express ();
 const path = require('path');
 const mainRouter = require('./routes/mainRouter');
 const productRouter = require('./routes/productRouter');
+const methodOverride = require('method-override');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './views'))
 
 app.use(express.static('public'));
-app.use('/', mainRouter)
-app.use('/product', productRouter)
+app.use('/', mainRouter);
+app.use('/product', productRouter);
+app.use(methodOverride('_method'));
 
 app.listen(3000, ()=> {console.log('Servidor arriba en el puerto 3000');})

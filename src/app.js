09942894a8ebdad9afef001077815fbe2c91140c
Ramón López <49588613+itutil@users.noreply.github.com>
2022-07-09 19:48:57 -0300

    Database config

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const { body, checkSchema, check, validationResult } = require('express-validato
 
 const mainRouter = require('./routes/mainRouter');
 const productRouter = require('./routes/productRouter');
+const userRouter = require('./routes/userRouter');
 
 /* Define application variables */
 
@@ -104,6 +105,20 @@ app.use('/', mainRouter);
 app.use('/product', productRouter);
 app.use('/producto', productRouter);
 
+
+/* Configure user flow */
+
+app.use(express.json());
+app.use(userRouter);
+// Handling Errors
+app.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+});
 
 /* Let application start listeting requests */
 

1cb3a00de7748a133e2fdb879cf6d70be32cbf05
GuillermoFontana <guilleefontana@gmail.com>
2022-08-20 21:06:00 -0300

    formulario de contacto

---
+++ src/controllers/comoRealizarTuPedidoController.js
@@ -0,0 +1,7 @@
+const controller = {
+    index: (req, res) => {
+        res.render('comoRealizarTuPedido');
+    },
+}; 
+
+module.exports = controller;
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 const productController = require('../controllers/productController');
 const contactController = require('../controllers/contactController');
+const comoRealizarTuPedidoController = require('../controllers/comoRealizarTuPedidoController')
 
 router.get('/', mainController.index);
 router.get('/menu-detail', mainController.mainDetail);
@@ -11,5 +12,6 @@ router.get('/register', mainController.login);
 router.get('/productos', productController.index);
 router.get('/products', productController.index);
 router.get('/contacto', contactController.index);
+router.get('/como-realizar-tu-pedido',comoRealizarTuPedidoController.index);
 
 module.exports = router;

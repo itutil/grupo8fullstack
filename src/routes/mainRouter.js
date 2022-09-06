0209773861c378bc6b316a54317d1bc01f6957d7
GuillermoFontana <guilleefontana@gmail.com>
2022-09-05 23:48:10 -0300

    terminado home y footer

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -9,6 +9,7 @@ const storesController = require('../controllers/storesController');
 const comoRealizarTuPedidoController = require('../controllers/comoRealizarTuPedidoController')
 const preguntasFrecuentesController = require('../controllers/preguntasFrecuentesController');
 const terminosYCondicionesController = require('../controllers/terminosYCondicionesController');
+const pagosYEnviosController = require('../controllers/pagosYEnviosController');
 
 
 router.get('/', mainController.index);
@@ -24,5 +25,6 @@ router.get('/contacto', contactoController.index);
 router.get('/como-realizar-tu-pedido',comoRealizarTuPedidoController.index);
 router.get('/terminosYCondiciones',terminosYCondicionesController.index);
 router.get('/preguntasFrecuentes',preguntasFrecuentesController.index);
+router.get('/pagosYEnvios',pagosYEnviosController.index);
 
 module.exports = router;

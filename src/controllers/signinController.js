03fcbdad89e10a4e925b26dac8c328281b5ea253
Ramón López <itutil@live.com>
2022-09-07 20:36:57 -0300

    All issues solved

--- src/controllers/signinController.js
+++ src/controllers/signinController.js
@@ -5,7 +5,6 @@ const passport = require('../middleware/mypassport.js')
 
 const signinController = {
     index: async (req, res) => {
-        var loggedUser = req.session.inSession
         const body = req.body; // OKvar loggedUser = req.session.inSession
         const user = await User.findOne({where: { eMail : req.body.eMail},});        
         if (user) {
@@ -14,18 +13,23 @@ const signinController = {
             if (validPassword) {
               //res.status(200).json({ message: "Valid password" });
               req.session.inSession = user;
-              req.session.username = user.eMail;           
+              req.session.username = user.eMail;
+              var loggedUser = req.session.inSession;
+              res.render('index.ejs', {userName: ('Welcome ' + user.firstName + ' ' + user.lastName)});
+              //res.write('Welcome back, ' + user.firstName + ' ' + user.lastName + '!');
+              //res.end();
+              //res.redirect('/');
+              //console.log(req.session.inSession);           
               //res.render('/',{"loggedUser":req.session.inSession.firstName})
               //res.redirect('/doctor-dashboard.html',{"user":username);
               //res.status(200).redirect('/',{"loggedUser":req.session.inSession.firstName}); 
-              res.status(200).redirect('/');
-            } else {
-              res.status(400).redirect('/'); // this would be an error page or another error notification
+              } else {
+              return res.status(400).redirect('/'); // this would be an error page or another error notification
             }
           } else {
-            res.status(401).redirect('/'); // this would be an error page or another error notification
+            return res.status(401).redirect('/'); // this would be an error page or another error notification
     
-          } 
+          }
       },}
 
 module.exports = signinController;
